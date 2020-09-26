5c15c7b21a3e0fdcefb453f520cd8fa352eac429
FROZNSUN937-PC\froznsun937 <smithcory937@gmail.com>
2020-09-26 13:25:17 -0400

    New files for unreal pilgrimage project

---
+++ Source/PilgrimageUnreal/PilgrimageUnrealGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PilgrimageUnrealGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PILGRIMAGEUNREAL_API APilgrimageUnrealGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
